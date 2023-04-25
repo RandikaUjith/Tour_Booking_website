34c1db327a5e020caba86bc3849492f97e1e25d1
Randika <mmrukumara@gmail.com>
2023-04-25 22:37:13 +0100

    db pw removed

--- content/includes/dbconn.php
+++ content/includes/dbconn.php
@@ -3,7 +3,7 @@
 function getConnection() {
     define('DB_NAME', 'unn_w22037955');
     define('DB_USER', 'unn_w22037955');
-    define('DB_PASSWORD','Rand270289');
+    define('DB_PASSWORD','********');
     define('DB_HOST', 'localhost');
     $conn = mysqli_connect(DB_HOST, DB_USER,
         DB_PASSWORD, DB_NAME) or
